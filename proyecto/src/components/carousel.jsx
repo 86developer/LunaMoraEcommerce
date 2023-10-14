a396e05ba8eddf737ba7323a9d4b7ac11369a73c
Nahuel Nuñez <nunezspinelli8@gmail.com>
2023-10-13 21:47:24 -0300

    funcionalidad carousel ok

--- proyecto/src/components/carousel.jsx
+++ proyecto/src/components/carousel.jsx
@@ -1,13 +1,12 @@
-'use client';
-
 import React, { useState, useEffect } from 'react';
+import '../styles/carousel.css';
 
 function Carousel() {
     const [currentSlide, setCurrentSlide] = useState(0);
 
     const slides = [
         { id: 1, imageSrc: 'https://previews.123rf.com/images/irishasel/irishasel1802/irishasel180200270/95651205-bragas-de-algod%C3%B3n-a-rayas-y-sujetador-blanco-lencer%C3%ADa-femenina-en-el-fondo-de-madera-vista.jpg', altText: 'Slide 1' },
-        { id: 2, imageSrc: 'https://www.infobae.com/new-resizer/EGlxQmMw1vViOBZziFy5_a9Ii9c=/filters:format(webp):quality(85)/arc-anglerfish-arc2-prod-infobae.s3.amazonaws.com/public/CWIPYTBDMBBELDGQRAE6MNGTSE.jpg', altText: 'Slide 2' },
+        { id: 2, imageSrc: 'https://img.freepik.com/fotos-premium/arreglo-lenceria-femenina-bodegon_23-2150411322.jpg?w=740', altText: 'Slide 2' },
         { id: 3, imageSrc: 'https://www.shutterstock.com/shutterstock/photos/1718154814/display_1500/stock-photo-portrait-of-a-confident-and-sexy-female-model-with-brown-hair-wearing-black-lingerie-while-laying-1718154814.jpg', altText: 'Slide 3' },
     ];
 
@@ -20,31 +19,39 @@ function Carousel() {
     };
 
     useEffect(() => {
-        const interval = setInterval(nextSlide, 3000); // Cambia la imagen cada 3 segundos 
-        return () => {
-            clearInterval(interval);
-        };
-    }, [currentSlide]);
+        const interval = setInterval(() => {
+            //setCurrentSlide((prevSlide) => (prevSlide === slides.length - 1 ? 0 : prevSlide + 1));
+        },);
+
+        return () => clearInterval(interval);
+    }, []);
 
     return (
         <div className="carousel-container">
-            <button onClick={prevSlide} className="carousel-button left">
-                Previous
-            </button>
             <div className="carousel-content">
                 {slides.map((slide, index) => (
                     <div
                         key={slide.id}
                         className={`slide ${index === currentSlide ? 'active' : ''}`}
-                    >
-                        <img src={slide.imageSrc} alt={slide.altText} />
-                    </div>
+                        style={{
+                            backgroundImage: `url(${slide.imageSrc})`, // Establecer la imagen de fondo en línea con la URL proporcionada
+                            backgroundSize: 'cover', // Ajustar el tamaño de la imagen de fondo al contenedor
+                            backgroundPosition: 'center', // Alinear la imagen de fondo al centro
+                            height: '500px', // Ajusta la altura del banner según tus necesidades
+                            width: '100%', // Ajusta el ancho del banner según tus necesidades
+                            display: index === currentSlide ? 'block' : 'none', // Mostrar solo la imagen actual, ocultar las demás
+                        }}
+                    ></div>
                 ))}
             </div>
+            <button onClick={prevSlide} className="carousel-button left">
+                Previous
+            </button>
             <button onClick={nextSlide} className="carousel-button right">
                 Next
             </button>
         </div>
+
     );
 }
 
