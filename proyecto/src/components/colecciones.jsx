4bcf291405faa3760bccfdbf55eba5d035dfb650
Nahuel Nuñez <nunezspinelli8@gmail.com>
2023-11-26 22:25:49 -0300

    modificacion de texto descriptivo en colecciones

--- proyecto/src/components/colecciones.jsx
+++ proyecto/src/components/colecciones.jsx
@@ -1,6 +1,22 @@
 'use client';
 import React, { useState } from 'react';
 
+import {
+    Roboto_Condensed, Alex_Brush
+} from 'next/font/google';
+
+const roboto = Roboto_Condensed({
+    weight: ["300"],
+    style: ["normal"],
+    subsets: ["latin"],
+})
+const alex = Alex_Brush({
+    weight: ["400"],
+    style: ["normal"],
+    subsets: ["latin"],
+})
+
+
 function Colecciones() {
     const [isOpen, setIsOpen] = useState(false);
 
@@ -9,32 +25,32 @@ function Colecciones() {
     };
     const callouts = [
         {
-            name: 'Desk and Office',
-            description: 'Work from home accessories',
+            name: 'Algodon',
+            description: 'Si tenes problemas con algun tipo de tela sintetica esta es tu categoria.',
             imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg',
             imageAlt: 'Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug.',
             href: '#',
         },
         {
-            name: 'Self-Improvement',
-            description: 'Journals and note-taking',
+            name: 'Poliester',
+            description: 'Creaciones en las ultimas telas del mercado entra aca y conocelas.',
             imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-02.jpg',
             imageAlt: 'Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant.',
             href: '#',
         },
         {
-            name: 'Travel',
-            description: 'Daily commute essentials',
+            name: 'New Arrivals',
+            description: 'Nuevas creaciones, tenemos novedades en cada una de las categorias.',
             imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg',
             imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
             href: '#',
         },
     ]
     return (
-        <div className="bg-gray-100">
+        <div className="bg-fuchsia-300">
             <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                 <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
-                    <h2 className="text-2xl font-bold text-gray-900">Collecciones</h2>
+                    <h2 className={`text-5xl text-gray-900 ${alex.className}`}>Colecciones</h2>
 
                     <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
                         {callouts.map((callout) => (
@@ -46,13 +62,13 @@ function Colecciones() {
                                         className="h-full w-full object-cover object-center"
                                     />
                                 </div>
-                                <h3 className="mt-6 text-sm text-gray-500">
+                                <h3 className={`mt-6 text-3xl ${alex.className} text-black-300`}>
                                     <a href={callout.href}>
                                         <span className="absolute inset-0" />
                                         {callout.name}
                                     </a>
                                 </h3>
-                                <p className="text-base font-semibold text-gray-900">{callout.description}</p>
+                                <p className={`text-base ${roboto.className}`}>{callout.description}</p>
                             </div>
                         ))}
                     </div>
