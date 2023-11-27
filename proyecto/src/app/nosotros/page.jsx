41c0948a0ce378b65d4e18991f561469ab9a8ae2
Nahuel Nuñez <nunezspinelli8@gmail.com>
2023-11-26 22:23:51 -0300

    nosotros contenido y fuente

--- proyecto/src/app/nosotros/page.jsx
+++ proyecto/src/app/nosotros/page.jsx
@@ -1,11 +1,49 @@
 'use client';
+import React, { useState } from 'react';
+import {
+    Roboto_Condensed, Alex_Brush
+} from 'next/font/google';
+
+const roboto = Roboto_Condensed({
+    weight: ["300"],
+    style: ["normal"],
+    subsets: ["latin"],
+})
+const alex = Alex_Brush({
+    weight: ["400"],
+    style: ["normal"],
+    subsets: ["latin"],
+})
 
 export default function Nosotros() {
     return (
         <div >
-            <h1>Nosotros</h1>
-            <h1>Acerca de nosotros y nuestra empresa.</h1>
-            <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate minus atque quaerat non blanditiis voluptatibus odio iste, nesciunt error facere hic obcaecati eligendi unde illo explicabo voluptatum dolores consequatur et. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam quaerat voluptatem perspiciatis deserunt incidunt facere quasi repellat necessitatibus odit enim eligendi quia error tempore quisquam, natus beatae repudiandae tempora earum.</h1>
+            <div className="bg-fuchsia-300 container mx-auto p-4">
+                <h1 className={`text-4xl font-bold mb-6${alex.className}`}>Nosotros</h1>
+                <div className="flex flex-wrap justify-center gap-4">
+                    <div className="w-full lg:w-1/2 p-4">
+                        <img
+                            src="https://via.placeholder.com/600"
+                            alt="Team"
+                            className="w-full rounded-lg shadow-md"
+                        />
+                    </div>
+                    <div className="w-full lg:w-1/2 p-4">
+                        <p className="mb-4">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed varius
+                            aliquam eros, nec feugiat est tempor eget.
+                        </p>
+                        <p className="mb-4">
+                            Integer vehicula risus a elit dapibus, nec venenatis velit commodo.
+                            Vivamus at odio ullamcorper, facilisis quam at, fringilla dolor.
+                        </p>
+                        <p>
+                            Mauris tincidunt orci nec diam volutpat, sit amet pretium nulla
+                            fermentum.
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
