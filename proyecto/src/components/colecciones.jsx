e305474e1f7b18c9af94e3d4a5fc7ab5de2e5691
Nahuel Nuñez <nunezspinelli8@gmail.com>
2023-11-29 05:58:00 -0300

    estilos a colecciones

--- proyecto/src/components/colecciones.jsx
+++ proyecto/src/components/colecciones.jsx
@@ -47,31 +47,29 @@ function Colecciones() {
         },
     ]
     return (
-        <div className="bg-fuchsia-300">
-            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-                <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
-                    <h2 className={`text-5xl text-gray-900 ${alex.className}`}>Colecciones</h2>
+        <div className="mx-auto max-w-7xl px-4 pb-0 sm:px-6 lg:px-8">
+            <div className="mb-5 mt-5 px-5 pb-6 pt-5 max-w-2xl rounded-lg sm:py-13 lg:max-w-none lg:py-10 bg-fuchsia-200">
+                <h2 className={`text-5xl text-gray-400 ${alex.className}`}>Colecciones</h2>
 
-                    <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
-                        {callouts.map((callout) => (
-                            <div key={callout.name} className="group relative">
-                                <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
-                                    <img
-                                        src={callout.imageSrc}
-                                        alt={callout.imageAlt}
-                                        className="h-full w-full object-cover object-center"
-                                    />
-                                </div>
-                                <h3 className={`mt-6 text-3xl ${alex.className} text-black-300`}>
-                                    <a href={callout.href}>
-                                        <span className="absolute inset-0" />
-                                        {callout.name}
-                                    </a>
-                                </h3>
-                                <p className={`text-base ${roboto.className}`}>{callout.description}</p>
+                <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
+                    {callouts.map((callout) => (
+                        <div key={callout.name} className="group relative">
+                            <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
+                                <img
+                                    src={callout.imageSrc}
+                                    alt={callout.imageAlt}
+                                    className="h-full w-full object-cover object-center"
+                                />
                             </div>
-                        ))}
-                    </div>
+                            <h3 className={`mt-6 text-3xl text-gray-400 ${alex.className}`}>
+                                <a href={callout.href}>
+                                    <span className="absolute inset-0" />
+                                    {callout.name}
+                                </a>
+                            </h3>
+                            <p className={`text-base text-gray-400 ${roboto.className}`}>{callout.description}</p>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
