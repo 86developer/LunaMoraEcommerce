c898fc29316319b049e119e5952848297d4fee5c
Nahuel Nuñez <nunezspinelli8@gmail.com>
2023-11-29 11:08:23 -0300

    componente scrollcard

--- proyecto/src/components/scrollcard.jsx
+++ proyecto/src/components/scrollcard.jsx
@@ -6,7 +6,10 @@ export default function ScrollCard({ imgSrc, description, alignStart, fadeleft }
 
     return (
         <div className={`px-7 pb-6 flex ${alignStart ? 'justify-start' : 'justify-end'}`}
-            data-aos={`${fadeleft ? "fade-left" : "fade-right"}`}>
+            data-aos={`${fadeleft ? "fade-left" : "fade-right"}`}
+            data-aos-anchor="#example-anchor"
+            data-aos-offset="50"
+            data-aos-duration="1000">
             <div className="max-w-2xl mx-1 my-1 rounded-lg bg-green-200 lg:flex">
                 <a href="#" className="lg:w-1/2">
                     <div style={{ maxWidth: '300px' }}> {/* Ajusta el ancho máximo según sea necesario */}
