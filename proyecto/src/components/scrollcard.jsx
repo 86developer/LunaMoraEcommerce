f8637c69307af1844673b59d3fafc672172f8120
Nahuel Nuñez <nunezspinelli8@gmail.com>
2023-11-29 11:08:02 -0300

    componente scrollcard

---
+++ proyecto/src/components/scrollcard.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Image from 'next/image';
+
+export default function ScrollCard({ imgSrc, description, alignStart, fadeleft }) {
+
+
+    return (
+        <div className={`px-7 pb-6 flex ${alignStart ? 'justify-start' : 'justify-end'}`}
+            data-aos={`${fadeleft ? "fade-left" : "fade-right"}`}>
+            <div className="max-w-2xl mx-1 my-1 rounded-lg bg-green-200 lg:flex">
+                <a href="#" className="lg:w-1/2">
+                    <div style={{ maxWidth: '300px' }}> {/* Ajusta el ancho máximo según sea necesario */}
+                        <Image src={imgSrc} alt="Descripción de la imagen" layout="responsive" className="rounded-lg" />
+                    </div>
+                </a>
+                <div className="ml-1 lg:w-1/2 lg:pl-1">
+                    <p className="nosotros mr-4 mb-3 leading-5 text-gray-400">{description}</p>
+                </div>
+            </div>
+        </div>
+    );
+};
