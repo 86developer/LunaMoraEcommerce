5e90da2e5c0d3e58e418e38dc7f4f38e198bd176
Nahuel Nuñez <nunezspinelli8@gmail.com>
2023-12-04 12:21:25 -0300

    add styles

--- proyecto/src/components/footer.jsx
+++ proyecto/src/components/footer.jsx
@@ -4,12 +4,10 @@ import Link from 'next/link';
 
 export default function Footer() {
     return (
-        <footer className="footer bg-white shadow">
-            <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-                    © 2023 <Link href="/home" className="hover:underline">Luna Mora</Link>. Todos los derechos reservados.
-                </span>
-                <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+        <footer className="footer bg-orange-200 shadow">
+            <div className="w-full mx-auto max-w-screen-xl p-3 md:flex md:items-center ">
+
+                <ul className="flex flex-wrap items-center my-1 mr-8 text-sm font-medium text-amber-900 sm:mt-0">
                     <li>
                         <Link href="/nosotros" className="mr-4 hover:underline md:mr-6">Nosotros</Link>
                     </li>
@@ -23,6 +21,9 @@ export default function Footer() {
                         <Link href="/contacto" className="hover:underline">Contacto</Link>
                     </li>
                 </ul>
+                <span className="text-sm sm:text-center text-amber-900">
+                    © 2023 <Link href="/home" className="hover:underline">Luna Mora</Link>. Todos los derechos reservados.
+                </span>
             </div>
         </footer>
     );
