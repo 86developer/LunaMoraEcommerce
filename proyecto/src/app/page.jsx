c653f469336dcc55a9ca88bfad4b590afb5b2efe
Nahuel Nuñez <nunezspinelli8@gmail.com>
2023-12-04 12:10:58 -0300

    add nosotros, formulario delete aos

--- proyecto/src/app/page.jsx
+++ proyecto/src/app/page.jsx
@@ -1,38 +1,34 @@
 'use client';
-import React, { useEffect } from 'react';
+import React from 'react';
 import Carousel from '@/components/carousel';
 import Colecciones from '@/components/colecciones';
-import ScrollCard from '@/components/scrollcard';
+import Nosotros from '@/components/nosotros';
+import Formulario from '@/components/formulario';
 import img1 from 'public/img/imagen1.webp';
 import img2 from 'public/img/imagen2.webp';
 import img3 from 'public/img/imagen3.webp';
-import img4 from 'public/img/imagen1.webp'; // Asegúrate de que sea diferente a las primeras cuatro
-import 'aos/dist/aos.css'; // Importa los estilos CSS de AOS
-import AOS from 'aos';
+import img4 from 'public/img/imagen1.webp';
+
 import Logo from '@/components/logo';
 
 
 export default function Home() {
-    useEffect(() => {
-        // Inicializa AOS una vez que el componente ha montado
-        AOS.init();
-    }, []);
+
 
     return (
         <>
             <Carousel />
             <div className=''>
-                <p className='my-20 text-center  text-green-400 '>
+                <blockquote className='my-8 mx-8 italic text-center text-base font-bold text-amber-900 '>
                     "Acompáñame en este viaje, y juntos podremos explorar la belleza y la sensualidad en la lencería de diseño propio."
-                </p>
+                </blockquote>
             </div>
             <Colecciones />
-
-            <ScrollCard imgSrc={img1} description="En medio de la incertidumbre de la pandemia, nació mi pasión por la lencería de diseño propio. Cada prenda es una manifestación de creatividad y dedicación." fadeleft />
-            <ScrollCard imgSrc={img2} description="Cada pieza que diseño es única, desde el concepto hasta el último detalle. Me esfuerzo por garantizar que cada prenda sea cómoda, segura y estéticamente atractiva." alignStart />
-            <ScrollCard imgSrc={img3} description="La exclusividad es un pilar fundamental de nuestro proyecto. Las prendas que diseñamos son de edición limitada y confeccionadas de manera artesanal, lo que les confiere un toque único y especial." fadeleft />
-            <ScrollCard imgSrc={img4} description="Nuestro compromiso es ofrecer lencería que combine belleza y funcionalidad. Cada cliente que elige una de mis creaciones está adquiriendo algo más que una prenda; está adquiriendo una historia de pasión y esfuerzo, y un símbolo de calidad." alignStart />
-
+            <Nosotros imgSrc={img1} description="En medio de la incertidumbre de la pandemia, nació mi pasión por la lencería de diseño propio. Cada prenda es una manifestación de creatividad y dedicación." />
+            <Nosotros imgSrc={img2} description="Cada pieza que diseño es única, desde el concepto hasta el último detalle. Me esfuerzo por garantizar que cada prenda sea cómoda, segura y estéticamente atractiva." />
+            <Nosotros imgSrc={img3} description="La exclusividad es un pilar fundamental de nuestro proyecto. Las prendas que diseñamos son de edición limitada y confeccionadas de manera artesanal, lo que les confiere un toque único y especial." />
+            <Nosotros imgSrc={img4} description="Nuestro compromiso es ofrecer lencería que combine belleza y funcionalidad. Cada cliente que elige una de mis creaciones está adquiriendo algo más que una prenda; está adquiriendo una historia de pasión y esfuerzo, y un símbolo de calidad." />
+            <Formulario />
         </>
     )
 }
