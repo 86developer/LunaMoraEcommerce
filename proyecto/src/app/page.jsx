385aa6c6420bb7001068fac3f7e25a7768447d0f
Nahuel Nuñez <nunezspinelli8@gmail.com>
2023-12-06 02:28:20 -0300

    change img

--- proyecto/src/app/page.jsx
+++ proyecto/src/app/page.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 import Carousel from '@/components/carousel';
 import Colecciones from '@/components/colecciones';
 import Nosotros from '@/components/nosotros';
-import Formulario from '@/components/formulario';
+import Contacto from '@/components/contacto';
 import img1 from 'public/img/imagen1.webp';
 import img2 from 'public/img/imagen2.webp';
 import img3 from 'public/img/imagen3.webp';
-import img4 from 'public/img/imagen1.webp';
+import mujerbanner from 'public/img/mujerbanner.webp';
 
 import Logo from '@/components/logo';
 
@@ -25,10 +25,10 @@ export default function Home() {
             </div>
             <Colecciones />
             <Nosotros imgSrc={img1} description="En medio de la incertidumbre de la pandemia, nació mi pasión por la lencería de diseño propio. Cada prenda es una manifestación de creatividad y dedicación." />
-            <Nosotros imgSrc={img2} description="Cada pieza que diseño es única, desde el concepto hasta el último detalle. Me esfuerzo por garantizar que cada prenda sea cómoda, segura y estéticamente atractiva." />
-            <Nosotros imgSrc={img3} description="La exclusividad es un pilar fundamental de nuestro proyecto. Las prendas que diseñamos son de edición limitada y confeccionadas de manera artesanal, lo que les confiere un toque único y especial." />
-            <Nosotros imgSrc={img4} description="Nuestro compromiso es ofrecer lencería que combine belleza y funcionalidad. Cada cliente que elige una de mis creaciones está adquiriendo algo más que una prenda; está adquiriendo una historia de pasión y esfuerzo, y un símbolo de calidad." />
-            <Formulario />
+            <Nosotros imgSrc={img3} description="Cada pieza que diseño es única, desde el concepto hasta el último detalle. Me esfuerzo por garantizar que cada prenda sea cómoda, segura y estéticamente atractiva." />
+            <Nosotros imgSrc={img2} description="La exclusividad es un pilar fundamental de nuestro proyecto. Las prendas que diseñamos son de edición limitada y confeccionadas de manera artesanal, lo que les confiere un toque único y especial." />
+            <Nosotros imgSrc={mujerbanner} description="Nuestro compromiso es ofrecer lencería que combine belleza y funcionalidad. Cada cliente que elige una de mis creaciones está adquiriendo algo más que una prenda; está adquiriendo una historia de pasión y esfuerzo, y un símbolo de calidad." />
+            <Contacto />
         </>
     )
 }
