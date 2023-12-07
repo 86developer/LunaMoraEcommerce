fede957113310a9d0ca97de6513d1681b3a18e69
Nahuel Nuñez <nunezspinelli8@gmail.com>
2023-12-07 01:44:59 -0300

    remove id

--- proyecto/src/app/page.jsx
+++ proyecto/src/app/page.jsx
@@ -24,7 +24,8 @@ export default function Home() {
                 </blockquote>
             </div>
             <Colecciones />
-            <Nosotros imgSrc={img1} description="En medio de la incertidumbre de la pandemia, nació mi pasión por la lencería de diseño propio. Cada prenda es una manifestación de creatividad y dedicación." />
+
+            <Nosotros imgSrc={img1} description="En medio de la incertidumbre de la pandemia, nació mi pasión por la lencería de diseño propio. Cada prenda es una manifestación de creatividad y dedicación." id="nosotros-component" />
             <Nosotros imgSrc={img3} description="Cada pieza que diseño es única, desde el concepto hasta el último detalle. Me esfuerzo por garantizar que cada prenda sea cómoda, segura y estéticamente atractiva." />
             <Nosotros imgSrc={img2} description="La exclusividad es un pilar fundamental de nuestro proyecto. Las prendas que diseñamos son de edición limitada y confeccionadas de manera artesanal, lo que les confiere un toque único y especial." />
             <Nosotros imgSrc={mujerbanner} description="Nuestro compromiso es ofrecer lencería que combine belleza y funcionalidad. Cada cliente que elige una de mis creaciones está adquiriendo algo más que una prenda; está adquiriendo una historia de pasión y esfuerzo, y un símbolo de calidad." />
