f84c74b6dd6b846d32fad4a8e36c1bbeeda13d2c
Nahuel Nuñez <nunezspinelli8@gmail.com>
2023-12-10 22:44:07 -0300

    anclaje nosotros funcion smooth

--- proyecto/src/app/page.jsx
+++ proyecto/src/app/page.jsx
@@ -1,14 +1,14 @@
 'use client';
-import React from 'react';
+import { useRef } from 'react';
 import Carousel from '@/components/carousel';
 import Colecciones from '@/components/colecciones';
 import Nosotros from '@/components/nosotros';
 import Contacto from '@/components/contacto';
 
-
-
 export default function Home() {
 
+    const nosotrosRef = useRef(null);
+    const coleccionesRef = useRef(null);
 
     return (
         <>
@@ -18,12 +18,17 @@ export default function Home() {
                     &quot; Acompáñame en este viaje, y juntos podremos explorar la belleza y la sensualidad en la lencería de diseño propio. &quot;
                 </blockquote>
             </div>
-            <Colecciones />
+            <div id="colecciones-component" ref={coleccionesRef}>
+                <Colecciones />
+            </div>
 
-            <Nosotros index={1} />
-            <Nosotros index={2} />
-            <Nosotros index={3} />
-            <Nosotros index={4} />
+            <div id="nosotros-component" ref={nosotrosRef}>
+                <Nosotros index={1} />
+                <Nosotros index={2} />
+                <Nosotros index={3} />
+                <Nosotros index={4} />
+
+            </div>
 
             {/* <Nosotros imgSrc={ropabanner} description="En medio de la incertidumbre de la pandemia, nació mi pasión por la lencería de diseño propio. Cada prenda es una manifestación de creatividad y dedicación." id="nosotros-component" />
             <Nosotros imgSrc={img3} description="Cada pieza que diseño es única, desde el concepto hasta el último detalle. Me esfuerzo por garantizar que cada prenda sea cómoda, segura y estéticamente atractiva." />
--- proyecto/src/components/navbar.jsx
+++ proyecto/src/components/navbar.jsx
@@ -1,19 +1,20 @@
 'use client'
 import Link from 'next/link';
-import { Fragment, useState } from 'react'
+import { Fragment, useState, useEffect } from 'react'
 import { Dialog, Popover, Tab, Transition } from '@headlessui/react'
 import { Bars3Icon, MagnifyingGlassIcon, ShoppingBagIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import Image from 'next/image';
 import '../styles/navbar.css';
 
-const navigation = {
-    categories: [
+function handleScrollToNosotros() {
+    const nosotrosElement = document.getElementById('nosotros-component');
 
-    ],
-    pages: [
-        { name: 'Nosotros', href: '#nosotros-component' },
-        { name: 'Colecciones', href: '#colecciones-component' },
-    ],
+    if (nosotrosElement) {
+        window.scrollTo({
+            top: nosotrosElement.offsetTop,
+            behavior: 'smooth',
+        });
+    }
 }
 
 function classNames(...classes) {
@@ -64,76 +65,17 @@ export default function Example() {
                                 </div>
 
                                 {/* Links */}
-                                <Tab.Group as="div" className="mt-2">
-                                    <div className="border-b border-gray-200">
-                                        <Tab.List className="-mb-px flex space-x-8 px-4">
-                                            {navigation.categories.map((category) => (
-                                                <Tab
-                                                    key={category.name}
-                                                    className={({ selected }) =>
-                                                        classNames(
-                                                            selected ? 'border-indigo-600 text-indigo-600' : 'border-transparent text-gray-900',
-                                                            'flex-1 whitespace-nowrap border-b-2 px-1 py-4 text-base font-medium'
-                                                        )
-                                                    }
-                                                >
-                                                    {category.name}
-                                                </Tab>
-                                            ))}
-                                        </Tab.List>
-                                    </div>
-                                    <Tab.Panels as={Fragment}>
-                                        {navigation.categories.map((category) => (
-                                            <Tab.Panel key={category.name} className="space-y-10 px-4 pb-8 pt-10">
-                                                <div className="grid grid-cols-2 gap-x-4">
-                                                    {category.featured.map((item) => (
-                                                        <div key={item.name} className="group relative text-sm">
-                                                            <div className="aspect-h-1 aspect-w-1 overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75">
-                                                                <Image src={item.imageSrc} alt={item.imageAlt} className="object-cover object-center" />
-                                                            </div>
-                                                            <a href={item.href} className="mt-6 block font-medium text-gray-900">
-                                                                <span className="absolute inset-0 z-10" aria-hidden="true" />
-                                                                {item.name}
-                                                            </a>
-                                                            <p aria-hidden="true" className="mt-1">
-                                                                Shop now
-                                                            </p>
-                                                        </div>
-                                                    ))}
-                                                </div>
-                                                {category.sections.map((section) => (
-                                                    <div key={section.name}>
-                                                        <p id={`${category.id}-${section.id}-heading-mobile`} className="font-medium text-gray-900">
-                                                            {section.name}
-                                                        </p>
-                                                        <ul
-                                                            role="list"
-                                                            aria-labelledby={`${category.id}-${section.id}-heading-mobile`}
-                                                            className="mt-6 flex flex-col space-y-6"
-                                                        >
-                                                            {section.items.map((item) => (
-                                                                <li key={item.name} className="flow-root">
-                                                                    <a href={item.href} className="-m-2 block p-2 text-gray-500">
-                                                                        {item.name}
-                                                                    </a>
-                                                                </li>
-                                                            ))}
-                                                        </ul>
-                                                    </div>
-                                                ))}
-                                            </Tab.Panel>
-                                        ))}
-                                    </Tab.Panels>
-                                </Tab.Group>
-
                                 <div className="space-y-6 border-t border-gray-200 px-4 py-6">
-                                    {navigation.pages.map((page) => (
-                                        <div key={page.name} className="flow-root">
-                                            <a href={page.href} className="-m-2 block p-2 font-medium text-gray-900">
-                                                {page.name}
-                                            </a>
-                                        </div>
-                                    ))}
+                                    <div className="flow-root">
+                                        <a href="/nosotros" className="-m-2 block p-2 font-medium text-gray-900">
+                                            Nosotros
+                                        </a>
+                                    </div>
+                                    <div className="flow-root">
+                                        <a href="/colecciones" className="-m-2 block p-2 font-medium text-gray-900">
+                                            Colecciones
+                                        </a>
+                                    </div>
                                 </div>
 
                                 <div className="space-y-6 border-t border-gray-200 px-4 py-6">
@@ -175,13 +117,13 @@ export default function Example() {
 
                             {/* Logo */}
                             <div className="ml-4 flex lg:ml-0">
-                                <Link href="/">
-                                    <span className="sr-only">Luna Mora</span>
+                                <Link href="/" >
+                                    {/* <span className="sr-only">Luna Mora</span> */}
                                     <img
                                         style={{
                                             width: '110px',
                                             height: '67px',
-                                            objectfit: 'contain',
+                                            objectFit: 'contain',
                                         }}
                                         priority
                                         width={1453} // Ancho real del logo
@@ -194,111 +136,25 @@ export default function Example() {
 
                             {/* Flyout menus */}
                             <Popover.Group className="hidden lg:ml-8 lg:block lg:self-stretch">
-                                <div className="flex h-full space-x-8">
-                                    {navigation.categories.map((category) => (
-                                        <Popover key={category.name} className="flex">
-                                            {({ open }) => (
-                                                <>
-                                                    <div className="relative flex">
-                                                        <Popover.Button
-                                                            className={classNames(
-                                                                open
-                                                                    ? 'border-indigo-600 text-indigo-600'
-                                                                    : 'border-transparent text-gray-700 hover:text-gray-800',
-                                                                'relative z-10 -mb-px flex items-center border-b-2 pt-px text-sm font-medium transition-colors duration-200 ease-out'
-                                                            )}
-                                                        >
-                                                            {category.name}
-                                                        </Popover.Button>
-                                                    </div>
+                                <div className="ml-auto flex items-center">
+                                    <Link href="#" onClick={handleScrollToNosotros}>
 
-                                                    <Transition
-                                                        as={Fragment}
-                                                        enter="transition ease-out duration-200"
-                                                        enterFrom="opacity-0"
-                                                        enterTo="opacity-100"
-                                                        leave="transition ease-in duration-150"
-                                                        leaveFrom="opacity-100"
-                                                        leaveTo="opacity-0"
-                                                    >
-                                                        <Popover.Panel className="absolute inset-x-0 top-full text-sm text-gray-500">
-                                                            {/* Presentational element used to render the bottom shadow, if we put the shadow on the actual panel it pokes out the top, so we use this shorter element to hide the top of the shadow */}
-                                                            <div className="absolute inset-0 top-1/2 bg-white shadow" aria-hidden="true" />
-
-                                                            <div className="relative bg-white">
-                                                                <div className="mx-auto max-w-7xl px-8">
-                                                                    <div className="grid grid-cols-2 gap-x-8 gap-y-10 py-16">
-                                                                        <div className="col-start-2 grid grid-cols-2 gap-x-8">
-                                                                            {category.featured.map((item) => (
-                                                                                <div key={item.name} className="group relative text-base sm:text-sm">
-                                                                                    <div className="aspect-h-1 aspect-w-1 overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75">
-                                                                                        <Image
-                                                                                            src={item.imageSrc}
-                                                                                            alt={item.imageAlt}
-                                                                                            className="object-cover object-center"
-                                                                                        />
-                                                                                    </div>
-                                                                                    <a href={item.href} className="mt-6 block font-medium text-gray-900">
-                                                                                        <span className="absolute inset-0 z-10" aria-hidden="true" />
-                                                                                        {item.name}
-                                                                                    </a>
-                                                                                    <p aria-hidden="true" className="mt-1">
-                                                                                        Shop now
-                                                                                    </p>
-                                                                                </div>
-                                                                            ))}
-                                                                        </div>
-                                                                        <div className="row-start-1 grid grid-cols-3 gap-x-8 gap-y-10 text-sm">
-                                                                            {category.sections.map((section) => (
-                                                                                <div key={section.name}>
-                                                                                    <p id={`${section.name}-heading`} className="font-medium text-gray-900">
-                                                                                        {section.name}
-                                                                                    </p>
-                                                                                    <ul
-                                                                                        role="list"
-                                                                                        aria-labelledby={`${section.name}-heading`}
-                                                                                        className="mt-6 space-y-6 sm:mt-4 sm:space-y-4"
-                                                                                    >
-                                                                                        {section.items.map((item) => (
-                                                                                            <li key={item.name} className="flex">
-                                                                                                <a href={item.href} className="hover:text-gray-800">
-                                                                                                    {item.name}
-                                                                                                </a>
-                                                                                            </li>
-                                                                                        ))}
-                                                                                    </ul>
-                                                                                </div>
-                                                                            ))}
-                                                                        </div>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                                                        </Popover.Panel>
-                                                    </Transition>
-                                                </>
-                                            )}
-                                        </Popover>
-                                    ))}
-
-                                    {navigation.pages.map((page) => (
-                                        <Link key={page.name} href={page.href} className="flex items-center text-sm font-medium text-gray-700 hover:text-gray-800 cursor-pointer">
-                                            {page.name}
-                                        </Link>
-                                    ))}
+                                        <div className="flex items-center text-sm font-medium text-gray-700 hover:text-gray-800 cursor-pointer" legacyBehavior>
+                                            <span>Nosotros</span>
+                                        </div>
+                                    </Link>
+                                    <Link href="#colecciones">
+                                        <div className="flex items-center text-sm font-medium text-gray-700 hover:text-gray-800 cursor-pointer" legacyBehavior>
+                                            <span>Colecciones</span>
+                                        </div>
+                                    </Link>
+                                    {/* Otros elementos del menú */}
                                 </div>
                             </Popover.Group>
-
-                            <div className="ml-auto flex items-center">
-
-
-
-
-
-                            </div>
                         </div>
                     </div>
                 </nav>
             </header>
         </div>
-    )
+    );
 }
