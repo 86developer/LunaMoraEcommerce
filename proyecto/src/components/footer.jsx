3107e787d18538174f3b5c281d66ba1ff221df58
Nahuel Nuñez <nunezspinelli8@gmail.com>
2023-12-15 02:41:20 -0300

    anclajes

--- proyecto/src/components/footer.jsx
+++ proyecto/src/components/footer.jsx
@@ -1,21 +1,52 @@
 import React from 'react';
 import Link from 'next/link';
 
+function handleScrollToNosotros(e) {
+    e.preventDefault();
+    const nosotrosElement = document.getElementById('nosotros-component');
+
+    if (nosotrosElement) {
+        window.scrollTo({
+            top: nosotrosElement.offsetTop,
+            behavior: 'smooth',
+        });
+    }
+}
+
+function handleScrollToColecciones(e) {
+    e.preventDefault();
+    const coleccionesElement = document.getElementById('colecciones-component');
+
+    if (coleccionesElement) {
+        window.scrollTo({
+            top: coleccionesElement.offsetTop,
+            behavior: 'smooth',
+        });
+    }
+}
 
 export default function Footer() {
+
+
+
     return (
         <footer className="footer bg-orange-200 shadow">
             <div className="w-full mx-auto max-w-screen-xl p-3 md:flex md:items-center ">
 
                 <ul className="flex flex-wrap items-center my-1 mr-8 text-sm font-medium text-amber-900 sm:mt-0">
                     <li>
-                        <Link href="/nosotros" className="mr-4 hover:underline md:mr-6">Nosotros</Link>
+                        <Link href="#" onClick={handleScrollToNosotros}>
+
+                            <div className="mr-4 hover:underline md:mr-6">
+                                <span>Nosotros</span>
+                            </div>
+                        </Link>
                     </li>
                     <li>
-                        <Link href="/colecciones" className="mr-4 hover:underline md:mr-6">Colecciones</Link>
+                        <Link href="#" onClick={handleScrollToColecciones} className="mr-4 hover:underline md:mr-6">Colecciones</Link>
                     </li>
                     <li>
-                        <a href="#" className="mr-4 hover:underline md:mr-6">Licencia</a>
+                        <a href="/#" className="mr-4 hover:underline md:mr-6">Licencia</a>
                     </li>
                     <li>
                         <Link href="/contacto" className="hover:underline">Contacto</Link>
--- proyecto/src/components/navbar.jsx
+++ proyecto/src/components/navbar.jsx
@@ -17,6 +17,17 @@ function handleScrollToNosotros() {
     }
 }
 
+function handleScrollToColecciones() {
+    const coleccionesElement = document.getElementById('colecciones-component');
+
+    if (coleccionesElement) {
+        window.scrollTo({
+            top: coleccionesElement.offsetTop,
+            behavior: 'smooth',
+        });
+    }
+}
+
 function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
 }
@@ -27,93 +38,16 @@ export default function Example() {
     return (
         <div className="bg-white">
             {/* Mobile menu */}
-            <Transition.Root show={open} as={Fragment}>
-                <Dialog as="div" className="relative z-40 lg:hidden" onClose={setOpen}>
-                    <Transition.Child
-                        as={Fragment}
-                        enter="transition-opacity ease-linear duration-300"
-                        enterFrom="opacity-0"
-                        enterTo="opacity-100"
-                        leave="transition-opacity ease-linear duration-300"
-                        leaveFrom="opacity-100"
-                        leaveTo="opacity-0"
-                    >
-                        <div className="fixed inset-0 bg-black bg-opacity-25" />
-                    </Transition.Child>
 
-                    <div className="fixed inset-0 z-40 flex">
-                        <Transition.Child
-                            as={Fragment}
-                            enter="transition ease-in-out duration-300 transform"
-                            enterFrom="-translate-x-full"
-                            enterTo="translate-x-0"
-                            leave="transition ease-in-out duration-300 transform"
-                            leaveFrom="translate-x-0"
-                            leaveTo="-translate-x-full"
-                        >
-                            <Dialog.Panel className="relative flex w-full max-w-xs flex-col overflow-y-auto bg-white pb-12 shadow-xl">
-                                <div className="flex px-4 pb-2 pt-5">
-                                    <button
-                                        type="button"
-                                        className="relative -m-2 inline-flex items-center justify-center rounded-md p-2 text-gray-400"
-                                        onClick={() => setOpen(false)}
-                                    >
-                                        <span className="absolute -inset-0.5" />
-                                        <span className="sr-only">Close menu</span>
-                                        <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-                                    </button>
-                                </div>
-
-                                {/* Links */}
-                                <div className="space-y-6 border-t border-gray-200 px-4 py-6">
-                                    <div className="flow-root">
-                                        <a href="/nosotros" className="-m-2 block p-2 font-medium text-gray-900">
-                                            Nosotros
-                                        </a>
-                                    </div>
-                                    <div className="flow-root">
-                                        <a href="/colecciones" className="-m-2 block p-2 font-medium text-gray-900">
-                                            Colecciones
-                                        </a>
-                                    </div>
-                                </div>
-
-                                <div className="space-y-6 border-t border-gray-200 px-4 py-6">
-                                    <div className="flow-root">
-                                        <a href="#" className="-m-2 block p-2 font-medium text-gray-900">
-                                            Sign in
-                                        </a>
-                                    </div>
-                                    <div className="flow-root">
-                                        <a href="#" className="-m-2 block p-2 font-medium text-gray-900">
-                                            Create account
-                                        </a>
-                                    </div>
-                                </div>
-
-                            </Dialog.Panel>
-                        </Transition.Child>
-                    </div>
-                </Dialog>
-            </Transition.Root>
 
             <header className="relative bg-white">
                 <p className="bg-orange-200 flex h-10 items-center justify-center px-4 text-lg font-medium text-amber-900 sm:px-6 lg:px-8">
                     BLACK FRIDAY!! 50% OFF
                 </p>
 
-                <nav aria-label="Top" className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <nav aria-label="Top" className="mx-auto max-w-7xl sm:px-6 lg:px-8">
                     <div className="border-b border-gray-200">
                         <div className="flex h-16 items-center">
-                            <button
-                                type="button"
-                                className="relative rounded-md bg-white p-2 text-gray-400 lg:hidden"
-                                onClick={() => setOpen(true)}
-                            >
-                                <span className="absolute -inset-0.5" />
-                                <span className="sr-only">Open menu</span>
-                                <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-                            </button>
 
                             {/* Logo */}
                             <div className="ml-4 flex lg:ml-0">
@@ -135,17 +69,16 @@ export default function Example() {
                             </div>
 
                             {/* Flyout menus */}
-                            <Popover.Group className="hidden lg:ml-8 lg:block lg:self-stretch">
-                                <div className="ml-auto flex items-center">
+                            <Popover.Group className=" lg:ml-8 lg:block lg:self-stretch">
+                                <div className="flex m-4">
                                     <Link href="#" onClick={handleScrollToNosotros}>
-
                                         <div className="flex items-center text-sm font-medium text-gray-700 hover:text-gray-800 cursor-pointer" legacyBehavior>
-                                            <span>Nosotros</span>
+                                            <p>Nosotros</p>
                                         </div>
                                     </Link>
-                                    <Link href="#colecciones">
-                                        <div className="flex items-center text-sm font-medium text-gray-700 hover:text-gray-800 cursor-pointer" legacyBehavior>
-                                            <span>Colecciones</span>
+                                    <Link href="#" onClick={handleScrollToColecciones}>
+                                        <div className="flex ml-4 items-center text-sm font-medium text-gray-700 hover:text-gray-800 cursor-pointer" legacyBehavior>
+                                            <p>Colecciones</p>
                                         </div>
                                     </Link>
                                     {/* Otros elementos del menú */}
